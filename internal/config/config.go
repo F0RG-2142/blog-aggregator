abbb6faedf295ec43f5924fcebd72f166f046f66
Louis <vanaaardtlj03@gmail.com>
2025-04-17 20:28:04 +0200

    completed SetUser function

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"path/filepath"
 )
 
 type Config struct {
@@ -10,17 +12,43 @@ type Config struct {
 	CurrentUserName string `json:"current_user_name"`
 }
 
-func Read() Config {
+func Read() (Config, error) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		panic("Could not find home directory")
+		return Config{}, fmt.Errorf("could not find home directory")
 	}
-	configFilePath := homeDir + "/.gatorconfig.json"
+	configFilePath := filepath.Join(homeDir, ".gatorconfig.json")
 	configFile, err := os.ReadFile(configFilePath)
 	if err != nil {
-		panic("Could not read config file: " + err.Error())
+		return Config{}, fmt.Errorf("could not read config file")
 	}
 	var currentConfig Config
-	json.Unmarshal(configFile, &currentConfig)
-	return currentConfig
+	if err := json.Unmarshal(configFile, &currentConfig); err != nil {
+		return Config{}, fmt.Errorf("could not unmarshal config file")
+	}
+	return currentConfig, nil
+}
+
+func SetUser(user string) error {
+	config, err := Read()
+	if err != nil {
+		return fmt.Errorf("could not read config file")
+	}
+	config.CurrentUserName = user
+	if user == config.CurrentUserName {
+		return nil
+	}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("could not find home directory: %v", err)
+	}
+	configFilePath := filepath.Join(homeDir, ".gatorconfig.json")
+	configData, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		return fmt.Errorf("could not marshal updated config file: %v", err)
+	}
+	if err := os.WriteFile(configFilePath, configData, 0644); err != nil {
+		return fmt.Errorf("could not write config file: %v", err)
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"os"
+
 	"github.com/F0RG-2142/blog-aggregator/internal/config"
 )
 
 func main() {
-	config.Read()
+	body, _ := config.Read()
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	currentUser := scanner.Text()
+	if body.CurrentUserName != currentUser {
+		config.SetUser(currentUser)
+	}
 }
